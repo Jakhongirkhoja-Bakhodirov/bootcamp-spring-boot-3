03c56b801dce73f86245c497dd8bd605e228059e
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-02-21 21:02:04 +0500

    use primary annotation to inject dependency

--- src/main/java/com/jakhongir/springboot/demo/mycoolapp/common/SwimmingCoach.java
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/common/SwimmingCoach.java
@@ -1,8 +1,10 @@
 package com.jakhongir.springboot.demo.mycoolapp.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class SwimmingCoach implements Coach {
     @Override
     public String getDailyWorkout() {
