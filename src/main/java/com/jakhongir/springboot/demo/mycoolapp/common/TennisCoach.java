a0c6cdbe6c3015f5e2935f9b48150988d7c4b21a
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-02-24 17:45:28 +0500

    specify bean prototype scope

--- src/main/java/com/jakhongir/springboot/demo/mycoolapp/common/TennisCoach.java
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/common/TennisCoach.java
@@ -1,8 +1,11 @@
 package com.jakhongir.springboot.demo.mycoolapp.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TennisCoach implements Coach {
     public TennisCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
