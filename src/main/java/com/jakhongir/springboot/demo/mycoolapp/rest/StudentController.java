23a8746bae0d4065126e6d068afc457ea4559860
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-02-29 20:48:46 +0500

    get simple get students api

---
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/rest/StudentController.java
@@ -0,0 +1,24 @@
+package com.jakhongir.springboot.demo.mycoolapp.rest;
+
+import com.jakhongir.springboot.demo.mycoolapp.dao.StudentDAO;
+import com.jakhongir.springboot.demo.mycoolapp.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentController {
+
+    private StudentDAO studentDAO;
+
+    public StudentController(StudentDAO studentDAO) {
+        this.studentDAO = studentDAO;
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return studentDAO.findAll();
+    }
+}
