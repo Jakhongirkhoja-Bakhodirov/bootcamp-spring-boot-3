aed95849964aad5c899c37074270833d3979a9fa
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-03-01 04:35:36 +0500

    create rest api get student by id

--- src/main/java/com/jakhongir/springboot/demo/mycoolapp/rest/StudentController.java
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/rest/StudentController.java
@@ -3,6 +3,7 @@ package com.jakhongir.springboot.demo.mycoolapp.rest;
 import com.jakhongir.springboot.demo.mycoolapp.dao.StudentDAO;
 import com.jakhongir.springboot.demo.mycoolapp.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,14 @@ public class StudentController {
     public StudentController(StudentDAO studentDAO) {
         this.studentDAO = studentDAO;
     }
+
     @GetMapping("/students")
     public List<Student> getStudents() {
         return studentDAO.findAll();
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return studentDAO.findById(studentId);
     }
 }
