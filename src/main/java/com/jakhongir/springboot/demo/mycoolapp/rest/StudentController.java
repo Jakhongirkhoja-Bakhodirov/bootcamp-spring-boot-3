ce0ac9fb5016553fed101f49782d3a4c69e62fcb
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-03-01 05:11:53 +0500

    create global handler exception

--- src/main/java/com/jakhongir/springboot/demo/mycoolapp/rest/StudentController.java
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/rest/StudentController.java
@@ -2,8 +2,6 @@ package com.jakhongir.springboot.demo.mycoolapp.rest;
 
 import com.jakhongir.springboot.demo.mycoolapp.dao.StudentDAO;
 import com.jakhongir.springboot.demo.mycoolapp.entity.Student;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -30,26 +28,5 @@ public class StudentController {
             throw new StudentNotFoundException("Student id not found - " + studentId);
         }
         return student;
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exception) {
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exception.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    // add another exception handler ... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exception) {
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(exception.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
