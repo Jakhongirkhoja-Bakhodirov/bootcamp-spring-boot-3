801c99d0a38e54d687c0ea7b35101539cd1db934
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-03-02 22:08:31 +0500

    create employee entity and DAO for crud employees APIs

---
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/entity/Employee.java
@@ -0,0 +1,40 @@
+package com.jakhongir.springboot.demo.mycoolapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name", length = 60)
+    private String firstName;
+
+    @Column(name = "last_name", length = 60)
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "role")
+    private String role;
+
+    public Employee() {
+
+    }
+
+    public Employee(String firstName, String lastName, String email, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
