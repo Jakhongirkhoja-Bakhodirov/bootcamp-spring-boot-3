8a765c04380306df914c804935bfd86310f9bf6d
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-03-05 08:47:07 +0500

    clean up main java file

--- src/main/java/com/jakhongir/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,9 +1,5 @@
 package com.jakhongir.springboot.demo.mycoolapp;
 
-import com.jakhongir.springboot.demo.mycoolapp.dao.EmployeeDAO;
-import com.jakhongir.springboot.demo.mycoolapp.dao.StudentDAO;
-import com.jakhongir.springboot.demo.mycoolapp.entity.Employee;
-import com.jakhongir.springboot.demo.mycoolapp.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,47 +14,8 @@ public class MycoolappApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(StudentDAO studentDAO, EmployeeDAO employeeDAO) {
+    public CommandLineRunner commandLineRunner() {
         return runner -> {
-         //   createStudent(studentDAO);
-          //  createEmployee(employeeDAO);
-//            readStudent(studentDAO);
-//            queryForStudents(studentDAO);
-//            updateStudent(studentDAO);
-//            deleteStudent(studentDAO);
-//            deleteAllStudent(studentDAO);
         };
-    }
-
-    private void createEmployee(EmployeeDAO employeeDAO) {
-        Employee employee = new Employee("Test", "Test", "testw@gmail.com", "developer");
-        employeeDAO.updateOrCreate(employee);
-    }
-
-    private void deleteAllStudent(StudentDAO studentDAO) {
-        System.out.println("Deleting all students");
-        studentDAO.deleteAll();
-    }
-
-    private void deleteStudent(StudentDAO studentDAO) {
-        int studentId = 2;
-        System.out.println("Getting student with id: " + studentId);
-        studentDAO.delete(studentId);
-    }
-
-    private void updateStudent(StudentDAO studentDAO) {
-        // retrieve student based on the id: primary key
-        int studentId = 1;
-        System.out.println("Getting student with id: " + studentId);
-        Student student = studentDAO.findById(studentId);
-
-        // change first name of student
-        student.setFirstName("Tom");
-
-        // update the student
-        studentDAO.update(student);
-
-        // display the updated student
-        System.out.println("Updated student: " + student);
     }
 }
