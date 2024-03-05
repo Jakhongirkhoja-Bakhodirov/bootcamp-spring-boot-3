be513c94bc08cadbb519e31e8ffde9a2d0a3dff1
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-03-05 21:04:48 +0500

    restrict APIs based on roles

--- src/main/java/com/jakhongir/springboot/demo/mycoolapp/security/AppSecurityConfig.java
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/security/AppSecurityConfig.java
@@ -2,18 +2,47 @@ package com.jakhongir.springboot.demo.mycoolapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppSecurityConfig {
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
+        UserDetails employee = User.builder()
+                .username("employee")
                 .password("{noop}test123").roles("EMPLOYEE").build();
 
-        return new InMemoryUserDetailsManager(john);
-    };
+        UserDetails manager = User.builder()
+                .username("manager")
+                .password("{noop}test123").roles("MANAGER").build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}test123").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(employee, manager, admin);
+    }
+
+    ;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configure ->
+                configure
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
+        );
+        //Use HTTP basic authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
 }
