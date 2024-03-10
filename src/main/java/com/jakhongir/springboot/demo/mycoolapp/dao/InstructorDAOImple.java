9c05575bbbe8ddd2818f305cce291a8f95a361d1
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-03-10 19:45:18 +0500

    add missing @Repository annotation to InstructorDAOImple

--- src/main/java/com/jakhongir/springboot/demo/mycoolapp/dao/InstructorDAOImple.java
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/dao/InstructorDAOImple.java
@@ -3,8 +3,10 @@ package com.jakhongir.springboot.demo.mycoolapp.dao;
 import com.jakhongir.springboot.demo.mycoolapp.entity.Instructor;
 import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public class InstructorDAOImple implements InstructorDAO {
 
     private EntityManager entityManager;
