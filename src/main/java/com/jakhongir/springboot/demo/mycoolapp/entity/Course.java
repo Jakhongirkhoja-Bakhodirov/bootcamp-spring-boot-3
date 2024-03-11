c724bf419c4f009c8d7e3add9ec8baa0b8dbd989
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-03-11 09:28:49 +0500

    create Course entity class in order to implement OneToMany relation

---
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/entity/Course.java
@@ -0,0 +1,45 @@
+package com.jakhongir.springboot.demo.mycoolapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+}
