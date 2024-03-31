581c67a3350de67a1d037143558b1d822b28524f
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-03-31 09:39:44 +0500

    pointcut declaration on any methods in dao package

--- src/main/java/com/jakhongir/springboot/demo/mycoolapp/aspect/LoggingAspect.java
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/aspect/LoggingAspect.java
@@ -2,18 +2,32 @@ package com.jakhongir.springboot.demo.mycoolapp.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
-//    @Before("execution(public void add*(com.jakhongir.springboot.demo.mycoolapp.entity.Employee,boolean isActive))")
-//    public void beforeAddAccount() {
-//        System.out.println("Before add account log something");
+    @Before("execution(public void add*(com.jakhongir.springboot.demo.mycoolapp.entity.Employee, ..))")
+    public void beforeAddAccount() {
+        System.out.println("Before add account log something");
+    }
+
+//    @Before("execution(* com.jakhongir.springboot.demo.mycoolapp.dao.*.*(..))")
+//    public void beforeAnyMethodInDaoPackage() {
+//        System.out.println("Before any method in dao package run this aspect logging !");
 //    }
 
-    @Before("execution(* com.jakhongir.springboot.demo.mycoolapp.dao.*.*(..))")
-    public void beforeAnyMethodInDaoPackage() {
-        System.out.println("Before any method in dao package run this aspect logging !");
+    @Pointcut("execution(* com.jakhongir.springboot.demo.mycoolapp.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Before("forDaoPackage()")
+    public void performApiAnalysis() {
+        System.out.println("Perform api analysis !");
+    }
+
+    @Before("forDaoPackage()")
+    public void addingLogs() {
+        System.out.println("Adding logs !");
     }
 }
