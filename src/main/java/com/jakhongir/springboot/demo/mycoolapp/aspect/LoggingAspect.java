65bd6186c3752938a9ac07583768c3e5585b3855
Jakhongir Bakhodirov <bahodirovjahongirxoja@gmail.com>
2024-04-01 23:58:16 +0500

    read method signature and params on aspect using JoinPoint

--- src/main/java/com/jakhongir/springboot/demo/mycoolapp/aspect/LoggingAspect.java
+++ src/main/java/com/jakhongir/springboot/demo/mycoolapp/aspect/LoggingAspect.java
@@ -1,8 +1,10 @@
 package com.jakhongir.springboot.demo.mycoolapp.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +12,17 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(2)
 public class LoggingAspect {
-//    @Before("execution(public void add*(com.jakhongir.springboot.demo.mycoolapp.entity.Employee, ..))")
-//    public void beforeAddAccount() {
-//        System.out.println("Before add account log something");
-//    }
+    @Before("execution(public void add*(com.jakhongir.springboot.demo.mycoolapp.entity.Employee, ..))")
+    public void beforeAddAccount(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Method" + methodSignature);
+
+        Object[] args = joinPoint.getArgs();
+        for (Object object : args) {
+            System.out.println("Object " + object);
+        }
+        System.out.println("Before add account log something");
+    }
 
 //    @Before("execution(* com.jakhongir.springboot.demo.mycoolapp.dao.*.*(..))")
 //    public void beforeAnyMethodInDaoPackage() {
@@ -35,7 +44,8 @@ public class LoggingAspect {
     }
 
     @Pointcut("forDaoPackage() && !(getter() || setter())")
-    private void forDaoPackageNoSetterNoGetter() {}
+    private void forDaoPackageNoSetterNoGetter() {
+    }
 
 //    @Before("forDaoPackageNoSetterNoGetter()")
 //    public void performApiAnalysis() {
